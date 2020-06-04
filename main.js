d18ed69fe678743d1cd9c7993bc7d9a38a105941
zanettepr <paulo.zanette@soaexpert.com.br>
2020-06-04 17:10:41 -0300

    Implementação do mock postPessoa

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ Sandbox.define('/pessoa', 'GET', (req, res) => {
     const pessoa = require('./pessoa/mock/getPessoa.js');
     pessoa.getPessoa(req, res);
 });
+
+// Usando parâmetro do body nas validações
+Sandbox.define('/pessoa', 'POST', (req, res) => {
+    const pessoa = require('./pessoa/mock/postPessoa.js');
+    pessoa.postPessoa(req, res);
+});
---
+++ pessoa/mock/postPessoa.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+    postPessoa: (req, res) => {
+
+        const validacao = require('../../utils/validacoes.js');
+
+        if(!validacao.verificarContentType(req)) {
+            return res.send(400,
+                {
+                    "message": 'Request body deve ser application/json!'
+                }
+            );
+        }
+
+        const dadosResponse = require('../data/postPessoa.js');
+
+        switch(req.body.pessoaId) {
+            case 1:
+                return res.send(dadosResponse.postPessoaAtiva());
+            case 2:
+                return res.send(dadosResponse.postPessoaInativa());
+            default:
+                return res.send(404,
+                    {
+                        message: 'Pessoa Id nao encontrado!'
+                    }
+                );
+        }
+    }
+}
